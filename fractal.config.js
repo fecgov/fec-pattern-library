5c35b4c6fb20d6624e47e2c1776451e864970ed0
tmpayton <121631201+tmpayton@users.noreply.github.com>
2023-05-16 16:59:04 -0400

    test directory change

--- fractal.config.js
+++ fractal.config.js
@@ -16,7 +16,7 @@ fractal.components.set('path', path.join(__dirname, 'components'));
 
 //Tell Fractal where to look for documentation pages.
 
-fractal.docs.set('path', path.join(__dirname, 'documentation'));
+fractal.docs.set('path', path.join(__dirname, 'docs'));
 
 //Tell the Fractal web preview plugin where to look for static assets.
 fractal.web.set('static.path', path.join(__dirname, 'public'));
